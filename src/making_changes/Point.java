104ac61530588c412ace9f64a5f64cfa6f2a0a8f
zahramadhar <zamadhar@gmail.com>
2023-01-10 22:00:07 -0800

    created new 3D Pointer (x,y,z) coordinates class

--- src/making_changes/Point.java
+++ src/making_changes/Point.java
@@ -3,9 +3,32 @@ package making_changes;
 public class Point {
     private int x;
     private int y;
+    private int z;
 
-    public Point(int x, int y) {
+    public Point(int x, int y, int z) {
         this.x = x;
         this.y = y;
+        this.z = z;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getZ() {
+        return z;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                ", z=" + z +
+                '}';
     }
 }
